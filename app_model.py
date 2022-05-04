70a4b156a73c19573802411871e9eaa483d166dd
AlfonsoEdMS <98701973+AlfonsoEdMS@users.noreply.github.com>
2022-05-04 19:53:39 +0200

    Update app_model.py

--- app_model.py
+++ app_model.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
-import os
 import pickle
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+# from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Lasso
 
 # os.chdir(os.path.dirname(__file__))
 
@@ -10,11 +14,12 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for Running a ML Algorithm to Predict Sales Revenue.</p>"
-model = pickle.load(open('ad_model.pkl','rb'))
 
-@app.route('/api/v1/predict', methods=['GET'])
+model = pickle.load(open('/home/AlfonsoEdlMS/Prueba_API/ad_model.pkl','rb'))
+
+@app.route('/api/v0/predict', methods=['GET'])
 def predict():
-    
+
     tv = request.get_json()['tv']
     radio = request.get_json()['radio']
     newspaper = request.get_json()['newspaper']
@@ -23,7 +28,25 @@ def predict():
         return "Args empty, the data are not enough to predict"
     else:
         prediction = model.predict([[tv,radio,newspaper]])
-    
+
     return jsonify({'predictions': prediction[0]})
+
+@app.route('/api/v0/retrain/', methods=['PUT'])
+def retrain():
+    archivo = request.get_json()['name']
+    data = pd.read_csv('/home/AlfonsoEdlMS/Prueba_API/' + archivo, index_col=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
+                                                    data['sales'],
+                                                    test_size = 0.20,
+                                                    random_state=42)
+
+    model = Lasso(alpha=6000)
+    model.fit(X_train, y_train)
+
+    pickle.dump(model, open('/home/AlfonsoEdlMS/Prueba_API/prueba.pkl', 'wb'))
+    # mean = mean_squared_error(y_test, model.predict(X_test))
+    # rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
+    return 'Sucess!'
 
 app.run()
